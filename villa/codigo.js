51345b39b5c0a6e90fc0d6315b721e9c1475bc77
JLob Lap <joselobatof@gmail.com>
2020-07-19 22:53:35 -0500

    Reto de cerdito en movimiento

--- villa/codigo.js
+++ villa/codigo.js
@@ -1,22 +1,118 @@
 var villa = document.getElementById("villa");
 var papel = villa.getContext("2d");
-var mapa = "tile.png";
 
-var fondo = new Image();
-fondo.src = mapa;
-fondo.addEventListener("load", dibujar);
+var teclas = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39
+};
 
-var vaca = new Image();
-vaca.src = "vaca.png";
+var fondo = {
+    url: "tile.png",
+    cargaOK: false
+}
 
-var pollo = new Image();
-pollo.src = "pollo.png";
+var vaca = {
+    url: "vaca.png",
+    cargaOK: false,
+    cantidad: aleatorio(1, 10)
+}
 
-var cerdo = new Image();
-cerdo.src = "cerdo.png";
+var pollo = {
+    url: "pollo.png",
+    cargaOK: false,
+    cantidad: aleatorio(1, 10)
+}
+
+var cerdo = {
+    url: "cerdo.png",
+    cargaOK: false,
+    cantidad: 1,
+    x: aleatorio(0,7) * 80,
+    y: aleatorio(0,7) * 80
+}
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVaca);
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollo);
+
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdo);
+
+function cargarFondo(){
+    fondo.cargaOK = true;
+    dibujar();
+}
+
+function cargarVaca(){
+    vaca.cargaOK = true;
+    dibujar();
+}
+
+function cargarPollo(){
+    pollo.cargaOK = true;
+    dibujar();
+}
+
+function cargarCerdo(){
+    cerdo.cargaOK = true;
+    dibujar();
+}
 
 function dibujar(){
-    papel.drawImage(fondo,0,0);
+    if(fondo.cargaOK){
+        papel.drawImage(fondo.imagen,0,0);
+    }
+    if(vaca.cargaOK){
+        for(var i=0; i<vaca.cantidad;i++){
+            var x = aleatorio(0,7) * 60;
+            var y = aleatorio(0,7) * 60;
+            papel.drawImage(vaca.imagen,x,y);
+        }
+    }
+    if(pollo.cargaOK){
+        for(var i=0; i<pollo.cantidad;i++){
+            var x = aleatorio(0,7) * 60;
+            var y = aleatorio(0,7) * 60;
+            papel.drawImage(pollo.imagen,x,y);
+        }
+    }
+    if(cerdo.cargaOK){            
+        papel.drawImage(cerdo.imagen,cerdo.x,cerdo.y);
+    }
+}
+
+document.addEventListener("keydown", moverCerdo);
+
+function moverCerdo(evento)
+{
+  var movimiento = 5;
+  switch(evento.keyCode)
+  {
+    case teclas.UP:
+      cerdo.y = cerdo.y - movimiento;
+    break;
+    case teclas.DOWN:
+      cerdo.y = cerdo.y + movimiento;
+    break;
+    case teclas.LEFT:
+      cerdo.x = cerdo.x - movimiento;
+    break;
+    case teclas.RIGHT:
+      cerdo.x = cerdo.x + movimiento;
+    break;
+  }
+  dibujar();
 }
 
 function aleatorio(min, max){
